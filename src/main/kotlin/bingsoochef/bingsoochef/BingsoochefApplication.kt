5952fb4584f9b0e0becd821cf86196fe559fed48
HyeonJeong <wgwjh05169@gmail.com>
2024-07-15 19:05:39 +0900

    init: 프로젝트 초기화

---
+++ src/main/kotlin/bingsoochef/bingsoochef/BingsoochefApplication.kt
@@ -0,0 +1,11 @@
+package bingsoochef.bingsoochef
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BingsoochefApplication
+
+fun main(args: Array<String>) {
+	runApplication<BingsoochefApplication>(*args)
+}
---
+++ src/test/kotlin/bingsoochef/bingsoochef/BingsoochefApplicationTests.kt
@@ -0,0 +1,13 @@
+package bingsoochef.bingsoochef
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BingsoochefApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
